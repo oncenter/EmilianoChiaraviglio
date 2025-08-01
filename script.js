579b71ff36cdcacb28dfa03119194b27c644971a
oncenter <tatolucero07@gmail.com>
2025-07-31 23:09:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script para toggle menú hamburguesa en móvil
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
